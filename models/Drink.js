882d5ff94c297de94b650871466177342c0c12bb
andrewheartsXD <andrewheartsxd@users.noreply.github.com>
2015-02-23 15:39:48 -0800

    troubleshooting server and routes

---
+++ models/Drink.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var drinkSchema = mongoose.Schema({
+  //Type of Drink
+  drinkName: String,
+  //Drink Ingredients
+  drinkRecipe: String,
+  //Drink Picture
+  drinkPicture: String
+});
+
+module.exports = mongoose.model('Drink', drinkSchema);
