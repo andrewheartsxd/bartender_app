e16fe547b4173f8c5c541ee32fb76fa47454dd0c
Brandon Burns <brandon10688@gmail.com>
2015-02-23 16:24:31 -0800

    new bar routes

--- routes/barRoutes.js
+++ routes/barRoutes.js
@@ -10,16 +10,36 @@ var bodyparser = require('body-parser');
 module.exports = function(app) {
   app.use(bodyparser.json());
 
-  app.get('/cheers', function(req, res) {
+// Drink
+
+  app.get('/cheers/drink', function(req, res) {
     Drink.find({}, function(err, data) {
       if (err) return res.status(500).send({'msg': 'could not retrieve drinks'});
       res.json(data);
     });
   });
 
-  app.post('/cheers', function(req, res) {
-    var newDrink = new DrinkOrder(req.body);
+  app.post('/cheers/drink', function(req, res) {
+    var newDrink = new Drink(req.body);
     newDrink.save(function(err, data) {
+      if (err) return res.status(500).send({'msg': 'could not save drink'});
+      
+      res.json(data);
+    });
+  });
+
+// Drink Order
+
+  app.get('/cheers/order', function(req, res) {
+    DrinkOrder.find({}, function(err, data) {
+      if (err) return res.status(500).send({'msg': 'could not retrieve drink orders'});
+      res.json(data);
+    });
+  });
+
+  app.post('/cheers/drinkorder', function(req, res) {
+    var newDrinkOrder = new DrinkOrder(req.body);
+    newDrinkOrder.save(function(err, data) {
       if (err) return res.status(500).send({'msg': 'could not save drink'});
       
       res.json(data);
@@ -36,26 +56,45 @@ module.exports = function(app) {
     });
   });
 
+// Bartender
+
+  app.get('/cheers/bartender', function(req, res) {
+    Bartender.find({}, function(err, data) {
+      if (err) return res.status(500).send({'msg': 'could not retrieve drink orders'});
+      res.json(data);
+    });
+  });
+
+  app.post('/cheers/bartender', function(req, res) {
+    var newBartender = new Bartender(req.body);
+    newBartender.save(function(err, data) {
+      if (err) return res.status(500).send({'msg': 'could not save drink'});
+      
+      res.json(data);
+    });
+  });
+
+
+// Queue
+
+  app.get('/cheers/queue', function(req, res) {
+    Queue.find({}, function(err, data) {
+      if (err) return res.status(500).send({'msg': 'could not retrieve drink orders'});
+      res.json(data);
+    });
+  });
+
+  app.post('/cheers/queue', function(req, res) {
+    var newQueue = new Queue(req.body);
+    newQueue.save(function(err, data) {
+      if (err) return res.status(500).send({'msg': 'could not save drink'});
+      
+      res.json(data);
+    });
+  });
+
 };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
 
