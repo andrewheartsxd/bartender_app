8dbe186d43adebd124b71e2bc65463081d761083
Brandon Burns <brandon10688@gmail.com>
2015-02-26 10:09:35 -0800

    fixed server.js

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ require('./routes/userRoutes')(userRouter, passport, app.get('appSecret'));
 app.use('/api/v1', barRouter);
 app.use('/api/v1', userRouter);
 
-app.listen(3000, function() {
+app.listen((process.env.PORT || 3000), function() {
   console.log('server listening on port ' + (process.env.PORT || 3000));
 });
