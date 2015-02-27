73ab4c067710570d44f9714830cb452eb4d7b879
andrewheartsXD <andrewheartsxd@users.noreply.github.com>
2015-02-26 17:57:36 -0800

    disabled picture save

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,17 +18,16 @@ module.exports = function(app, passport, appSecret) {
       if (err) return res.status(500).send({msg: 'could not create user'});
 
     //saves profile picture to /public folder as _id.jpg
-      User.find({'basic.email': newUser.basic.email}, function(err, data) {
-        if (err) return res.status(500).send({msg: 'could not create user'});
-
-        var userMongoID = data[0]._id;
-        var imageBuff = new Buffer(req.body.userPic, 'utf8');
-        fs.writeFileSync('./public/' + userMongoID + '.jpg', imageBuff, 'binary');
-        imageBuff = null;
-        console.dir(userMongoID);
-      });
-
-       
+//      User.find({'basic.email': newUser.basic.email}, function(err, data) {
+//        if (err) return res.status(500).send({msg: 'could not create user'});
+//
+//        var userMongoID = data[0]._id;
+//        var imageBuff = new Buffer(req.body.userPic, 'utf8');
+//        fs.writeFileSync('./public/' + userMongoID + '.jpg', imageBuff, 'binary');
+//        imageBuff = null;
+//        console.dir(userMongoID);
+//      });
+//
       user.generateToken(appSecret, function(err, token) {
         if (err) res.status(500).send({msg: 'could not generate token'});
         res.json({eat: token});
